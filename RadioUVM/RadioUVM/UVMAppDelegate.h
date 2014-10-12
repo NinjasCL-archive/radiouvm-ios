f247ac6b43dbc44f0845cd7eb27d8c7e45b39f2d
clsource <camilo@cervezapps.cl>
2014-10-12 15:19:26 -0300

    Added skeleton initial files, can close #1

---
+++ RadioUVM/RadioUVM/UVMAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UVMAppDelegate.h
+//  RadioUVM
+//
+//  Created by Camilo Castro on 12-10-14.
+//  Copyright (c) 2014 Universidad de Viña del Mar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UVMAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RadioUVM/RadioUVM/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RadioUVM
+//
+//  Created by Camilo Castro on 12-10-14.
+//  Copyright (c) 2014 Universidad de Viña del Mar. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UVMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UVMAppDelegate class]));
+    }
+}
